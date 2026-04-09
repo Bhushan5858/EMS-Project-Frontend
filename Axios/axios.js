a69ff0e606fa19f18e82a5bbafe896dcb083a57c
Bhushan5858 <patilpavan5858@gmail.com>
2026-04-09 15:10:36 +0530

    base url set

--- Axios/axios.js
+++ Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const axiosInstance =axios.create({
 
-    baseURL:"http://localhost:5000",
+    baseURL:"https://ems-project-backend-production.up.railway.app",
     withCredentials:true
 });
 
