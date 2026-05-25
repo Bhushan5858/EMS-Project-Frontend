01513d4cc5ccb0df2640894ce6ba8117b8ddd4a4
Bhushan5858 <patilpavan5858@gmail.com>
2026-05-25 22:45:24 +0530

    backend url chnaged

--- Axios/axios.js
+++ Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "http://13.205.209.102:5000",
+  baseURL: "http://13.205.86.177:5000",
   withCredentials: true,
 });
 
